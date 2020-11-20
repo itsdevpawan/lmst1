9df85d8f17d6a2eeb9b1cd59751fd58ab63df02c
itsdevpawan <itsdevpawan@gmail.com>
2020-11-20 18:00:34 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,62 @@
+// search box js
+"use strict";
+
+var searchBox = document.querySelectorAll(
+  '.search-box input[type="text"] + span'
+);
+
+searchBox.forEach((elm) => {
+  elm.addEventListener("click", () => {
+    elm.previousElementSibling.value = "";
+  });
+});
+
+// achivement section carousel
+
+$(document).ready(function () {
+  $("#myCarousel").on("slide.bs.carousel", function (e) {
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 4;
+    var totalItems = $(".carousel-item").length;
+
+    if (idx >= totalItems - (itemsPerSlide - 1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i = 0; i < it; i++) {
+        // append slides to end
+        if (e.direction == "left") {
+          $(".carousel-item").eq(i).appendTo(".carousel-inner");
+        } else {
+          $(".carousel-item").eq(0).appendTo($(this).find(".carousel-inner"));
+        }
+      }
+    }
+  });
+});
+
+// modal js
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
